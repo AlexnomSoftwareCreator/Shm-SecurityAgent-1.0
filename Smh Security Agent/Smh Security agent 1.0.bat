2a7707efb7a9e7a91221a41c5799763ce96a4cea
SuitesSoftwareCreator <119679986+AlexnomSoftwareCreator@users.noreply.github.com>
2022-12-05 13:06:00 +0200

    Smh Security agent 1.0.bat

---
+++ Smh Security Agent/Smh Security agent 1.0.bat
@@ -0,0 +1,46 @@
+@Echo off
+Title Smh Security Agent 1.0
+Color 40
+Echo @Copyright 2022 Smh
+Echo help protected your pc from virus and adware and anything virus
+Echo press any key to start...
+Pause
+Echo Starting...
+Echo Scanning and deleting...
+Del Noescape.exe
+Del Spyheriff.exe
+Del Micro virus.exe
+Del Micro virus.bat
+Del Trojan horse.exe
+Del Trojan horse.bat
+Del Virus.exe
+Del Virus.bat
+Del Antivirus Installer.exe
+Del Antivirus.exe
+Del Time.exe
+Del Magicreverssi.zip
+Del Wannacry.exe
+Del Petya.exe
+Del Creeper.com
+Del Winpcap Installer.exe
+Del Alureon.exe
+Del Fun.exe
+Del Graybird.exe
+Del Mydoom.exe
+Del Navidad.exe
+Del Simile.exe
+Del ZMist.exe
+Del ILOVEYOU.exe
+Del Hare.exe
+Del Abraxas.exe
+Del Acid.exe
+Del Acme.exe
+Del Espeanto.exe
+Del SpywareNo.exe
+Del Pest Trap.exe
+Del SpyDemolisher.exe
+Del SpyFalcon.exe
+Echo Complete!
+Echo Press any key to exit
+Pause
+Exit
